55afdc3b7163f51322d91cf32be51cbf6780ca35
Shlomi Fish <shlomif@shlomifish.org>
2020-10-17 16:21:38 +0300

    avoid including Build.PL ; fix cpan

--- lib/Math/FFT.pm
+++ lib/Math/FFT.pm
@@ -13,7 +13,7 @@ require DynaLoader;
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
-our $VERSION = '1.35';
+our $VERSION = '1.36';
 
 bootstrap Math::FFT $VERSION;
 
